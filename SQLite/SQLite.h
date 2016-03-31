1bdab8ed527db18b342154e7f25aae0da5ae5b08
Robert Payne <robertpayne@me.com>
2016-03-31 13:55:55 +1300

    Initial Commit

---
+++ SQLite/SQLite.h
@@ -0,0 +1,20 @@
+//
+//  SQLite.h
+//  SQLite
+//
+//  Created by Robert Payne on 31/03/16.
+//  Copyright © 2016 Sense Medical. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SQLite.
+FOUNDATION_EXPORT double SQLiteVersionNumber;
+
+//! Project version string for SQLite.
+FOUNDATION_EXPORT const unsigned char SQLiteVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SQLite/PublicHeader.h>
+
+#import "sqlite3.h"
+#import "sqlite3ext.h"
